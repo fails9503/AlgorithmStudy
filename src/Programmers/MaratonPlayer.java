e7851c1f9f20995d08db2a0e6ed433cb26b050fd
Lux <fails9503@gmail.com>
2021-03-17 20:57:56 +0900

    2021-03-17 1. 들어오지 못한 선수 - MaratonPlayer - getOrDefault 수정 - 파이썬 생각

--- src/Programmers/MaratonPlayer.java
+++ src/Programmers/MaratonPlayer.java
@@ -5,18 +5,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 // getOrDefault를 활용하는 방법도 있음!
+// 파이썬은 딕셔너리로 푸는건가??
 public class MaratonPlayer {
 
     public String solution(String[] participant, String[] completion) {
 
         Map<String, Integer> hm = new HashMap<>();
 
-        for(String s : participant) {
-            if(hm.containsKey(s)) {
-                hm.put(s, hm.get(s)+1);
-            } else {
-                hm.put(s,1);
-            }
+        for(String player : participant) {
+
+            // hm에 값을 집어넣는데 키는 plyaer로 넣고 value는 만약에
+            // 키가 player인 값이 맵에 없으면 기본값 0+1해서 1이 들어가고 있으면 그값 +1
+            hm.put(player, hm.getOrDefault(player, 0) + 1);
+
+//            if(hm.containsKey(s)) {
+//                hm.put(s, hm.get(s)+1);
+//            } else {
+//                hm.put(s,1);
+//            }
+
         }
 
         for(String s : completion) {
